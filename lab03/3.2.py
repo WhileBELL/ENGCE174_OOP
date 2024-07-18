2efc964f2741190d1a5ddbdfcc9f38689dba3e13
WhileBELL <bellbelieve.work@gmail.com>
2024-07-18 20:42:41 +0700

    fix 3.2

--- lab03/3.2.py
+++ lab03/3.2.py
@@ -1,18 +1,14 @@
-def generate_patterns(n, current=0, results=None):
-    results1 = ""
-    results2 = ""
-    results3 = ""
-    results4 = ""
-    results5 = ""
-
-    if current == n:
+def generate_patterns(
+    n, current=1, results1="", results2="", results3="", results4="", results5=""
+):
+    if current > n:
         return results1, results2, results3, results4, results5
 
-    line1 = "*" * (current + 1)
-    line2 = " " * (n - current - 1) + "*" * (current + 1)
+    line1 = "*" * current
+    line2 = " " * (n - current) + "*" * current
     line3 = "*" * (n - current)
-    line4 = " " * (n - current - 1) + "* " * (current + 1)
-    line5 = " " * current + "* " * (n - current)
+    line4 = " " * (n - current) + "* " * current
+    line5 = " " * (current - 1) + "* " * (n - current + 1)
 
     results1 += line1 + "\n"
     results2 += line2 + "\n"
@@ -20,11 +16,13 @@ def generate_patterns(n, current=0, results=None):
     results4 += line4 + "\n"
     results5 += line5 + "\n"
 
-    return generate_patterns(n, current + 1, results)
+    return generate_patterns(
+        n, current + 1, results1, results2, results3, results4, results5
+    )
 
 
 def generateline(n):
-    print("-" * n)
+    print("-" * (n * 2))
 
 
 x = int(input("Enter a number (integer): "))
@@ -32,11 +30,11 @@ x = int(input("Enter a number (integer): "))
 pattern1, pattern2, pattern3, pattern4, pattern5 = generate_patterns(x)
 
 print(pattern1)
-generateline(x * 2)
+generateline(x)
 print(pattern2)
-generateline(x * 2)
+generateline(x)
 print(pattern3)
-generateline(x * 2)
+generateline(x)
 print(pattern4)
-generateline(x * 2)
+generateline(x)
 print(pattern5)
